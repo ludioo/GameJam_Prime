5969e8e51fdbbaf2e6fa307b92fe5cde44f6cfe0
ludioo <panahan23@gmail.com>
2023-07-13 14:45:21 +0700

    Adding PanelManager

--- Assets/_Barkah/Script/PanelManager.cs
+++ Assets/_Barkah/Script/PanelManager.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class PanelManager : MonoBehaviour
 {
-    
+    public void OpenPanel(GameObject panelToOpen)
+    {
+        panelToOpen.SetActive(true);
+        CanvasGroup panelCG = panelToOpen.AddComponent(typeof(CanvasGroup)) as CanvasGroup;
+        LeanTween.alphaCanvas(panelCG, 1, 0.5f);
+    }
+
+    public void ClosePanel(GameObject panelToClose)
+    {
+        CanvasGroup panelCG = panelToClose.AddComponent(typeof(CanvasGroup)) as CanvasGroup;
+        LeanTween.alphaCanvas(panelCG, 0, 0.5f).setOnComplete(() =>
+              panelToClose.SetActive(false));
+    }
 }
